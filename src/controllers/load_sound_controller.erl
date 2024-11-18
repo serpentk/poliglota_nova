f777db951afe6c30a0d695746e98bb5ca97391a3
Katerina N. Shananina <e.shananina@rambler-co.ru>
2024-11-19 00:53:32 +0300

    Load controller now works

--- src/controllers/load_sound_controller.erl
+++ src/controllers/load_sound_controller.erl
@@ -2,6 +2,18 @@
 -export([
          load_sound/1
         ]).
+-include_lib("track.hrl").
 
-load_sound(_Req) ->
-    {json, #{status => <<"ok">>}}.
+load_sound(#{json := #{<<"lang">> := Lang, <<"word">> := Word}}) ->
+    {ok, ApiKey} = application:get_env(poliglota_nova, forvo_key),
+    Track = forvo:request_forvo(Lang, Word, ApiKey),
+    {ok, DbEnv} = application:get_env(poliglota_nova, dbconfig),
+    Dbconfig = #{host => proplists:get_value(host, DbEnv), 
+                 username => proplists:get_value(username, DbEnv), 
+                 password => proplists:get_value(password, DbEnv), 
+                 database => proplists:get_value(database, DbEnv)},
+    trackdb:save_track(Track, Dbconfig),
+    {json, 200, #{}, #{path => list_to_binary(Track#track.location)}};
+load_sound(_Req) -> 
+{json, 400, #{}, #{error => <<"Wrong request params">>}}.
+
